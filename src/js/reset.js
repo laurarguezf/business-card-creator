2082c1580e311cfc3197124b5c59a4491ee5f6ba
Ale <alexlab84@hotmail.com>
2024-07-10 21:54:42 +0200

    add reset input and reset card

--- src/js/reset.js
+++ src/js/reset.js
@@ -1,18 +1,40 @@
 'use strict';
 
 const resetBtn = document.querySelector('.js__resetBtn');
-/* const nameInput = document.querySelector('.js__inputName'); */
+const mailInput = document.querySelector('.js__inputMail');
+const inputTlf = document.querySelector('.js__inputTlf');
+const inputLinkedin = document.querySelector('.js__inputLinkedin');
+const inputGithub = document.querySelector('.js__inputGithub');
+const previewTlf = document.querySelector('.js__previewTlf');
+const previewMail = document.querySelector('.js__previewMail');
+const previewLinkedin = document.querySelector('.js__previewLinkedin');
+const previewGithub = document.querySelector('.js__previewGithub');
+
 
 const handleClickResetInputCard = () => {
-
     resetInputs();
+    resetCard();
 }
 
 const resetInputs = () => {
     nameInput.value = '';
     jobInput.value = '';
-
+    mailInput.value = '';
+    inputTlf.value = '';
+    inputLinkedin.value = '';
+    inputGithub.value = '';
 }
+
+const resetCard = () => {
+    namePreview.innerHTML = `<h3 class="js__namePreview">Nombre Apellido</h3>`;
+    jobPreview.innerHTML = `<p class="js__jobPreview">Front-end developer</p>`;
+    previewTlf.innerHTML = `<a href="" class="js__previewTlf"><i class="fa-solid fa-mobile-screen-button preview_contact--icon"></i></a>`;
+    previewMail.innerHTML = `<a href="" class="js__previewMail"><i class="fa-regular fa-envelope preview_contact--icon"></i></a>`;
+    previewLinkedin.innerHTML = `<a href="" class="js__previewLinkedin"><i class="fa-brands fa-linkedin-in preview_contact--icon"></i></a>`
+    previewGithub.innerHTML = `<a href="" class="js__previewGithub"><i class="fa-brands fa-github-alt preview_contact--icon"></i></a>`
+}
+
+
 
 
 resetBtn.addEventListener ('click', handleClickResetInputCard);
