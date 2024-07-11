ffe4779edc5c5d3780cb3c4ccfed7d4ed30e524a
Ale <alexlab84@hotmail.com>
2024-07-11 22:28:42 +0200

    I add # in the links

--- src/js/reset.js
+++ src/js/reset.js
@@ -31,10 +31,10 @@ const resetInputs = () => {
 const resetCard = () => {
     namePreview.innerHTML = `<h3 class="js__namePreview">Nombre Apellido</h3>`;
     jobPreview.innerHTML = `<p class="js__jobPreview">Front-end developer</p>`;
-    previewTlf.innerHTML = `<a href="" class="js__previewTlf"><i class="fa-solid fa-mobile-screen-button preview_contact--icon"></i></a>`;
-    previewMail.innerHTML = `<a href="" class="js__previewMail"><i class="fa-regular fa-envelope preview_contact--icon"></i></a>`;
-    previewLinkedin.innerHTML = `<a href="" class="js__previewLinkedin"><i class="fa-brands fa-linkedin-in preview_contact--icon"></i></a>`;
-    previewGithub.innerHTML = `<a href="" class="js__previewGithub"><i class="fa-brands fa-github-alt preview_contact--icon"></i></a>`;
+    previewTlf.innerHTML = `<a href="#" class="js__previewTlf"><i class="fa-solid fa-mobile-screen-button preview_contact--icon"></i></a>`;
+    previewMail.innerHTML = `<a href="#" class="js__previewMail"><i class="fa-regular fa-envelope preview_contact--icon"></i></a>`;
+    previewLinkedin.innerHTML = `<a href="#" class="js__previewLinkedin"><i class="fa-brands fa-linkedin-in preview_contact--icon"></i></a>`;
+    previewGithub.innerHTML = `<a href="#" class="js__previewGithub"><i class="fa-brands fa-github-alt preview_contact--icon"></i></a>`;
     
 }
 
