3439634b7b6b22832ab67ba1eeea7cd4d1b9d177
evasanzgue <evasanzgue@gmail.com>
2024-07-11 18:28:52 +0200

    I have created the events and the media queries.

--- src/js/inputs.js
+++ src/js/inputs.js
@@ -3,7 +3,6 @@
     /* VARIABLES GLOBALES*/
 
     const data = {
-        palette: 1,
         name:'',
         job: '',
         phone: '',
@@ -23,6 +22,30 @@
     const jobInput = document.querySelector('.js__inputJob');
     const jobPreview = document.querySelector('.js__jobPreview');
 
+    const inputPhone = document.querySelector('.js__inputPhone');
+    const phonePreview = document.querySelector('.js__phonePreview');
+
+    const inputMail = document.querySelector('.js__inputMail')
+    const mailPreview = document.querySelector('.js__mailPreview');
+
+    const inputLinkedin = document.querySelector('.js__inputLinkedin');
+    const linkedinPreview = document.querySelector('.js__linkedinPreview');
+
+    const inputGitHub = document.querySelector('.js__inputGithub');
+    const gitGubPreview = document.querySelector('.js__gitHubPreview');
+
+    /* FUNCIONES */ 
+
+    const preview = () => {
+        namePreview.innerHTML = data.name;
+        jobPreview.innerHTML = data.job;
+
+        phonePreview.href = `${data.phone}`;
+        mailPreview.href = `${data.email}`;
+        linkedinPreview.href = `https://${data.linkedin}`;
+        gitGubPreview.href = `https://${data.github}`;
+    }
+    
 
     /* EVENTOS */
 
@@ -35,7 +58,7 @@
         if(data.name === '') {
             data.name = namePreviewDefaultValue;
         }
-        namePreview.innerHTML = data.name;
+        preview();
     };
 
     const handleInputJob = (ev) => {
@@ -43,10 +66,40 @@
         if(data.job === '') {
             data.job = jobPreviewDefaultValue;
         }
-        jobPreview.innerHTML = data.job;
+        preview();
     };
 
+    const handleInputPhone = (ev) => {
+        data.phone = inputPhone.value;
+        preview();
+    };
+
+    const handleInputEmail = (ev) => {
+        data.email = inputMail.value;
+        preview();
+    };
+
+    const handleInputLinkedin = (ev) => {
+        data.linkedin = inputLinkedin.value;
+        preview();
+    };
+
+    const handleInputGitHub = (eva) => {
+        data.github = inputGitHub.value;
+        preview();
+    }
 
 
     nameInput.addEventListener('input', handleInputName);
     jobInput.addEventListener('input' , handleInputJob);
+
+    inputPhone.addEventListener('input', handleInputPhone);
+    inputMail.addEventListener('input', handleInputEmail);
+    inputLinkedin.addEventListener('input', handleInputLinkedin);
+    inputGitHub.addEventListener('input', handleInputGitHub);
+    
+   
+
+
+    
+
