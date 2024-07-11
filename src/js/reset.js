c735fc192e8aad1ef1b6cc7432dd1756433d00b8
Ale <alexlab84@hotmail.com>
2024-07-11 20:23:13 +0200

    add reset button finished

--- src/js/reset.js
+++ src/js/reset.js
@@ -9,11 +9,14 @@ const previewTlf = document.querySelector('.js__previewTlf');
 const previewMail = document.querySelector('.js__previewMail');
 const previewLinkedin = document.querySelector('.js__previewLinkedin');
 const previewGithub = document.querySelector('.js__previewGithub');
+const previewCard = document.querySelector('.js__previewCard');
+
 
 
 const handleClickResetInputCard = () => {
     resetInputs();
     resetCard();
+    resetPallete();
 }
 
 const resetInputs = () => {
@@ -30,9 +33,27 @@ const resetCard = () => {
     jobPreview.innerHTML = `<p class="js__jobPreview">Front-end developer</p>`;
     previewTlf.innerHTML = `<a href="" class="js__previewTlf"><i class="fa-solid fa-mobile-screen-button preview_contact--icon"></i></a>`;
     previewMail.innerHTML = `<a href="" class="js__previewMail"><i class="fa-regular fa-envelope preview_contact--icon"></i></a>`;
-    previewLinkedin.innerHTML = `<a href="" class="js__previewLinkedin"><i class="fa-brands fa-linkedin-in preview_contact--icon"></i></a>`
-    previewGithub.innerHTML = `<a href="" class="js__previewGithub"><i class="fa-brands fa-github-alt preview_contact--icon"></i></a>`
+    previewLinkedin.innerHTML = `<a href="" class="js__previewLinkedin"><i class="fa-brands fa-linkedin-in preview_contact--icon"></i></a>`;
+    previewGithub.innerHTML = `<a href="" class="js__previewGithub"><i class="fa-brands fa-github-alt preview_contact--icon"></i></a>`;
+    
 }
+
+const resetPallete = () => {
+    previewCard.innerHTML = `<div class="js__previewCard">
+        <div class="preview_info">
+            <h3 class="info_name js__namePreview">Nombre Apellido</h3>
+            <p class="info_job js__jobPreview">Front-end developer</p>
+            <img class="info_img" src="./images/img_prueba_preview.jpg" alt="">
+        </div>
+        <nav class="preview_contact">
+            <a href="" class="preview_contact--link js__previewTlf"><i class="fa-solid fa-mobile-screen-button preview_contact--icon"></i></a>
+            <a href="" class="preview_contact--link js__previewMail"><i class="fa-regular fa-envelope preview_contact--icon"></i></a>
+            <a href="" class="preview_contact--link js__previewLinkedin"><i class="fa-brands fa-linkedin-in preview_contact--icon"></i></a>
+            <a href="" class="preview_contact--link js__previewGithub"><i class="fa-brands fa-github-alt preview_contact--icon"></i></a>
+        </nav>
+    </div>`;
+};
+
 
 
 
